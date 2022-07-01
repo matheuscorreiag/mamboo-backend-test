a60f143cd55d8f90512df9fc361e11250b54e7cb
Matheus Correia <matheuscorreiags@gmail.com>
2022-07-01 19:45:51 -0300

    feat: erros middleware

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import "express-async-errors";
+import { errorsMiddleware } from "./middlewares/errors";
 import { routes } from "./routes";
 
 const app = express();
 app.use(express.json());
 app.use(routes);
+app.use(errorsMiddleware);
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
