2c5898317f7ff6b0b47efeca98bc0b50e55f6d2a
Matheus Correia <matheuscorreiags@gmail.com>
2022-07-01 20:25:24 -0300

    feat: create task dto

--- src/modules/tasks/controllers/CreateTask.controller.ts
+++ src/modules/tasks/controllers/CreateTask.controller.ts
@@ -1,11 +1,13 @@
 import { Request, Response } from "express";
+import { CreateTaskDto } from "../dtos/CreateTask.dto";
 import { CreateTaskUseCase } from "../useCases/CreateTask.useCase";
 
 export class CreateTaskController {
   constructor(private createTaskUseCase: CreateTaskUseCase) {}
 
   async handle(request: Request, response: Response) {
-    const { title, description } = request.body;
+    const { title, description } = CreateTaskDto.parse(request.body);
+
     const task = await this.createTaskUseCase.execute({
       title,
       description,
