2bc012bb88d98393576827071ac4b1c1495d4d75
Matheus Correia <matheuscorreiags@gmail.com>
2022-07-03 17:23:33 -0300

    feat: cors config

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import cors from "cors";
 import express from "express";
 import "express-async-errors";
 import { errorsMiddleware } from "./middlewares/errors";
@@ -5,9 +6,11 @@ import { routes } from "./routes";
 
 const app = express();
 app.use(express.json());
+
+app.use(cors());
 app.use(routes);
 app.use(errorsMiddleware);
 
-app.listen(3000, () => {
+app.listen(3333, () => {
   console.log("Server is running on port 3000");
 });
